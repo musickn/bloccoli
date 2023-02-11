c842bb3f019e93ea3a90361cbb1013f8f50bd015
musicincircles <panasarn.kanchanapiboon@circles.asia>
2023-02-11 20:21:59 +0700

    chore: state before apply an api call

---
+++ app/src/main/java/com/example/broccoli/di/AppModule.kt
@@ -0,0 +1,69 @@
+package com.example.broccoli.di
+
+import com.example.broccoli.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient()
+            .newBuilder()
+            .callTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain ->
+                val requestBuilder = chain.request()
+                    .newBuilder()
+
+                val newRequest: Request = requestBuilder.build()
+                chain.proceed(newRequest)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiServices(retrofitClient: Retrofit): APIs {
+        return retrofitClient.create(APIs::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVideoRepository(
+        api: APIs
+    ): TmdbRepository {
+        val remoteDataSourceImpl = RemoteDataSourceImpl(api)
+        return TmdbRepository(remoteDataSourceImpl)
+    }
+}
---
+++ app/src/main/java/com/example/broccoli/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.broccoli.util
+
+object Constants {
+    const val BASE_URL = "https://us-central1-blinkapp-684c1.cloudfunctions.net/"
+    const val FAKEAUTH = "fakeAuth"
+}
